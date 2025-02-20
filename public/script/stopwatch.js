567de4c9b5d45319624adf6f173c7bf43c4e8069
yudhi1933 <yudhiyudhitok@gmail.com>
2025-02-20 13:38:06 +0700

    membuat stopwatch

---
+++ public/script/stopwatch.js
@@ -0,0 +1,17 @@
+setInterval(() => {
+    let date = new Date();
+    let jam = date.getHours();
+    let menit = date.getMinutes();
+    let detik = date.getSeconds();
+    let milidetik = date.getMilliseconds();
+
+    if (jam < 10) jam = "0" + jam;
+    if (menit < 10) menit = "0" + menit;
+    if (detik < 10) detik = "0" + detik;
+    if (milidetik < 10) milidetik = "0" + milidetik;
+
+    document.getElementById("jam").value = jam;
+    document.getElementById("menit").value = menit;
+    document.getElementById("detik").value = detik;
+    document.getElementById("milidetik").value = milidetik;
+}, 10);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        drakBlue:'#2b3748',
+      },
+    },
   },
   plugins: [],
 }
