42a9c80d5b429a91c930f619e7046e0a4b7d67e5
yudhi1933 <yudhiyudhitok@gmail.com>
2025-02-21 17:01:44 +0700

    menghapus milidetk di html dan membuat ulang java script

--- public/script/stopwatch.js
+++ public/script/stopwatch.js
@@ -1,4 +1,31 @@
-clearInterval(timer);
-console.log("Timer dihentikan");
-timer = null;
+let timer = null;
+let totalDetik = 0;
+
+function start() {
+    if (timer) clearInterval(timer);
+    timer = setInterval(function () {
+        totalDetik++;
+        let detik = totalDetik % 60;
+        let menit = Math.floor(totalDetik / 60);
+        let jam = Math.floor(totalDetik / 3600);
+        document.getElementById("jam").value = ("0" + jam).slice(-2);
+        document.getElementById("menit").value = ("0" + menit).slice(-2);
+        document.getElementById("detik").value = ("0" + detik).slice(-2);
+
+    }, 1000);
+    
+};
+
+function stop() {
+    clearInterval(timer);
+    console.log("Timer dihentikan");
+};
+
+function reset() {
+    clearInterval(timer);
+    totalDetik = 0;
+    document.getElementById("jam").value = "00";
+    document.getElementById("menit").value = "00";
+    document.getElementById("detik").value = "00";
+};
 
